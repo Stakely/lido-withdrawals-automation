fbc513beb002686666ba4a0794b2ab351e2c67fa
Anto <antoniotg96@gmail.com>
2023-03-27 13:47:48 +0200

    Fix: It has moved the constant out of the try.

--- functions/fetchValidatorsData.js
+++ functions/fetchValidatorsData.js
@@ -1,41 +1,42 @@
 const axios = require('axios');
 
 async function fetchValidatorsData(kapiUrl, moduleId, operatorId, percentage) {
-  try {
 
 	const completeKapiUrl = `${kapiUrl}/v1/modules/${moduleId}/validators/validator-exits-to-prepare/${operatorId}?percent=${percentage}`;
-    const response = await axios.get(completeKapiUrl, {
-		headers: {
-			'Content-Type': 'application/json',
-			Accept: 'application/json',
-		},
-	});
-		
-		let kapiJsonResponse = response.data;
 
-		// Check validators data length
-		if (kapiJsonResponse.data.length === 0) {
-		throw new Error('Data (validators) length from KAPI is 0');
+	try {
+		const response = await axios.get(completeKapiUrl, {
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+		});
+			
+			let kapiJsonResponse = response.data;
+
+			// Check validators data length
+			if (kapiJsonResponse.data.length === 0) {
+			throw new Error('Data (validators) length from KAPI is 0');
+			}
+
+			// Check validators data fields
+			for (const validator of kapiJsonResponse.data) {
+			if (!validator.validatorIndex || !validator.key) {
+				throw new Error('Data item is missing "validatorIndex" or "key" field');
+			}
+			}
+
+			// Check epoch is an integer
+			if (typeof kapiJsonResponse.meta.clBlockSnapshot.epoch !== 'number' || !Number.isInteger(kapiJsonResponse.meta.clBlockSnapshot.epoch)) {
+			throw new Error('Epoch field is missing or not an integer');
+			}
+
+			return kapiJsonResponse;
+
+		} catch (error) {
+			throw new Error('Failed to fetch data from the KAPI (Url: ' + completeKapiUrl + '). ' + error.message);
 		}
-
-		// Check validators data fields
-		for (const validator of kapiJsonResponse.data) {
-		if (!validator.validatorIndex || !validator.key) {
-			throw new Error('Data item is missing "validatorIndex" or "key" field');
-		}
-		}
-
-		// Check epoch is an integer
-		if (typeof kapiJsonResponse.meta.clBlockSnapshot.epoch !== 'number' || !Number.isInteger(kapiJsonResponse.meta.clBlockSnapshot.epoch)) {
-		throw new Error('Epoch field is missing or not an integer');
-		}
-
-		return kapiJsonResponse;
-
-  } catch (error) {
-    throw new Error('Failed to fetch data from the KAPI (Url: ' + completeKapiUrl + '). ' + error.message);
-  }
-}
+	}
 
 module.exports = {
   fetchValidatorsData,
